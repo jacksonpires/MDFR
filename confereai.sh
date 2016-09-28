726b6f1c37a6103962f74db6be962c5f8003b04c
Jonathan Soifer <jaysoifer@users.noreply.github.com>
2016-09-28 09:24:37 -0300

    open all the necessary tabs and extract functions
    title and color are best suited in .zshrc

--- confereai.sh
+++ confereai.sh
@@ -1,8 +1,24 @@
 #!/bin/zsh
+
+# For this script to work, you need to setup the title and color functions
+# on your .zshrc file
+# You also need to install npm install -g newtab
+source ~/.zshrc
+
+# RAILS SERVER Tab Setup
 cd ~/Developer/confere-ai-rails
-echo -ne "\033]0;SERVER\007"
-echo -e "\033]6;1;bg;red;brightness;270\a"
-echo -e "\033]6;1;bg;green;brightness;60\a"
-echo -e "\033]6;1;bg;blue;brightness;83\a"
-ttab -G bundle exec guard
-echo "test"
+title DEVELOPMENT
+color development
+/bin/rails s
+
+# BUNDLE GUARD Tab Setup
+ttab -G eval "title TESTS;color tests; bundle exec guard"
+
+# POSTGRES Tab Setup
+ttab -G eval "title DATABASE;color database; psql"
+
+# HEROKU Tab Setup
+ttab -G eval "title PRODUCTION;color production; heroku logs --tail"
+
+# GIT Tab Setup
+ttab -G eval "title VERSIONING;color versioning; git status"
