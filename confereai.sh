5f66aa5a71d6402e257e8410233016946b1e7374
Jonathan Soifer <jaysoifer@users.noreply.github.com>
2016-09-28 09:44:56 -0300

    properly set directories and make the script work

--- confereai.sh
+++ confereai.sh
@@ -1,24 +1,29 @@
 #!/bin/zsh
 
-# For this script to work, you need to setup the title and color functions
-# on your .zshrc file
-# You also need to install npm install -g newtab
+# Conditions
+# 01. Call the script using . confereai.sh (note the space)
+# 02. Define title function on your bash profile (".zshrc")
+# 03. Define color function on your bash profile (".zshrc")
+# 04. Have the proper software installed (Rails/Guard, Heroku Toolbelt, Postgres up and running, Git)
+# 05. Install the newtab npm package globally
 source ~/.zshrc
 
-# RAILS SERVER Tab Setup
-cd ~/Developer/confere-ai-rails
-title DEVELOPMENT
-color development
-/bin/rails s
+# Set the proper directory for new tabs
+cd ~/Developer/confere-ai-rails 
 
-# BUNDLE GUARD Tab Setup
+# GUARD Tab Setup for Tests
 ttab -G eval "title TESTS;color tests; bundle exec guard"
 
-# POSTGRES Tab Setup
+# POSTGRES Tab Setup as the Database
 ttab -G eval "title DATABASE;color database; psql"
 
-# HEROKU Tab Setup
+# HEROKU Tab Setup as the Production Environment
 ttab -G eval "title PRODUCTION;color production; heroku logs --tail"
 
-# GIT Tab Setup
+# GIT Tab Setup as the Versioning System
 ttab -G eval "title VERSIONING;color versioning; git status"
+
+# RAILS SERVER Tab Setup as the Development Environment
+title DEVELOPMENT
+color development
+cd ~/Developer/confere-ai-rails; ./bin/rails s
